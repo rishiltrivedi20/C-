698bb2549aa4610c489324d79178fe79f2e5a961
Rishil Trivedi <73719632+rishiltrivedi20@users.noreply.github.com>
2020-10-31 01:47:49 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a;
+    cin>>a;
+    if(a==1)
+    {
+    	cout<<"jati reje";	
+	}
+    else if(a%2==0){
+    	cout<<a<<" "<<"Prime";
+	}
+    else
+	{
+    	 cout<<a<<" "<<"Not prime";
+	}
+   
+}
